ff673e48a2a8dbc8c6528a09dffde1720b8e7423
Kasun Wilbagedara <kasunwilbagedara2001@gmail.com>
2025-04-21 22:07:27 +0530

    encoding generator

---
+++ EncodeGenerator.py
@@ -0,0 +1,40 @@
+import cv2
+import face_recognition
+import pickle
+import os
+
+
+#importing student images into a list
+folderPath = 'Images'
+pathList = os.listdir(folderPath)
+print(pathList)
+imgList = []
+studentsIds = []
+
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath,path)))
+
+    studentsIds.append(os.path.splitext(path)[0])
+   # print(path)
+    #print(os.path.splitext(path)[0])
+
+    print(studentsIds)
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+
+    return encodeList
+
+print("Encoding Started....")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown, studentsIds]
+
+print("Encoding Complete")
+
+file=open("EncodeFile.p",'wb')
+pickle.dump(encodeListKnownWithIds,file)
+file.close()
